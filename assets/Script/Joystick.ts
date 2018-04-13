80ed90819849ad4553c098516e3e94d8353d7af2
limichange <limichange@hotmail.com>
2018-04-13 12:00:52 +0800

    Update

--- assets/Script/Joystick.ts
+++ assets/Script/Joystick.ts
@@ -2,6 +2,7 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Joystick extends cc.Component {
+
   @property(cc.Node)
   panel: cc.Node = null
 
@@ -10,17 +11,14 @@ export default class Joystick extends cc.Component {
 
   angle: number = null
   power: number = 0
+  radius: number = 0
 
   start() {
-    this.panel.on(cc.Node.EventType.TOUCH_START, this.onTouchStart, this)
+    this.radius = this.node.width / 2
+    this.panel.on(cc.Node.EventType.TOUCH_START, this.onTouchMove, this)
     this.panel.on(cc.Node.EventType.TOUCH_MOVE, this.onTouchMove, this)
     this.panel.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this)
     this.panel.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this)
-  }
-
-  onTouchStart(e: cc.Event) {
-    e.stopPropagation()
-    this.setHandlePosition(e.touch.getLocation())
   }
 
   onTouchMove(e: cc.Event) {
@@ -31,8 +29,11 @@ export default class Joystick extends cc.Component {
   setHandlePosition(touchPosition: cc.Vec2) {
     let position = this.panel.convertToNodeSpaceAR(touchPosition)
 
-    if (position.mag() > this.node.width / 2) {
-      position = position.normalizeSelf().mulSelf(this.node.width / 2)
+    if (position.mag() > this.radius) {
+      position = position.normalizeSelf().mulSelf(this.radius)
+      this.power = 1
+    } else {
+      this.power = position.mag() / this.radius
     }
 
     this.angle = position.angle(cc.p(0, 1))
